c188eef03fb6f532b13846aaa466fef1b12ea1a2
Jennifer Johnson <jenjeep7@hotmail.com>
2017-04-03 13:36:26 -0500

    testing started

--- src/app/supplies/supplies.component.spec.ts
+++ src/app/supplies/supplies.component.spec.ts
@@ -1,13 +1,19 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SuppliesComponent } from './supplies.component';
+import { MaterialModule } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 describe('SuppliesComponent', () => {
   let component: SuppliesComponent;
   let fixture: ComponentFixture<SuppliesComponent>;
+  let material: MaterialModule;
+  let flex: FlexLayoutModule;
+
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [MaterialModule, FlexLayoutModule],
       declarations: [ SuppliesComponent ]
     })
     .compileComponents();
@@ -22,4 +28,31 @@ describe('SuppliesComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('Should return supplies', () => {
+
+    expect(component.supplies).toEqual(
+      [{
+        type: 'flowers',
+        instock: 8,
+        description: 'Inseason Plants of various sizes',
+        Price: 13.99,
+        featured: false
+
+      },
+      {
+      type: 'trees',
+      instock: 22,
+      description: 'Various Trees',
+      Price: 50,
+      featured: true
+    },
+    {
+      type: 'potting soil',
+      instock: 0,
+      description: 'Potting soil',
+      Price: 5.99,
+      featured: false
+    }])
+  })
 });
--- src/app/supplies/supplies.component.ts
+++ src/app/supplies/supplies.component.ts
@@ -11,20 +11,23 @@ export class SuppliesComponent implements OnInit {
     type: 'flowers',
     instock: 8,
     description: 'Inseason Plants of various sizes',
-    Price: 13.99
+    Price: 13.99,
+    featured: false
 
   },
   {
   type: 'trees',
   instock: 22,
   description: 'Various Trees',
-  Price: 50
+  Price: 50,
+  featured: true
 },
 {
   type: 'potting soil',
   instock: 0,
   description: 'Potting soil',
-  Price: 5.99
+  Price: 5.99,
+  featured: false
 }];
 
 totalItems() {
